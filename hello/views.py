5de03fc59cb12c2ae19ab700943ba47ac7720431
CBesser <cbesser@seas.upenn.edu>
2018-02-12 16:46:23 -0500

    removed spaces

--- hello/views.py
+++ hello/views.py
@@ -21,27 +21,27 @@ app = Flask(__name__)
 # Route for adding image to S3
 # Returns an HTML render
 def add_image():
-    return render_template('upload_image.html')
+	return render_template('upload_image.html')
 
 @app.route('/sign_s3/')
 # Sign s3 request
 # Returns the response dump
 def sign_s3():
-  	S3_BUCKET = os.environ.get('S3_BUCKET')
+	S3_BUCKET = os.environ.get('S3_BUCKET')
 	file_name = request.args.get('file_name')
 	file_type = request.args.get('file_type')
 	s3 = boto3.client('s3')
 	presigned_post = s3.generate_presigned_post(Bucket = S3_BUCKET_NAME, Key = file_name,
-	    Fields = {"acl": "public-read", "Content-Type": file_type},
-	    Conditions = [
-	   		{"acl": "public-read"},
-		    {"Content-Type": file_type}
-	    ],
-	    ExpiresIn = 3600
+		Fields = {"acl": "public-read", "Content-Type": file_type},
+		Conditions = [
+			{"acl": "public-read"},
+			{"Content-Type": file_type}
+		],
+		ExpiresIn = 3600
 	)
 	return json.dumps({
-	    'data': presigned_post,
-	    'url': 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET_NAME, file_name)
+		'data': presigned_post,
+		'url': 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET_NAME, file_name)
 	})
 
 @app.route("/submit_form/", methods = ["POST"])
@@ -56,7 +56,7 @@ def submit_form():
 # Param: request - HTTP client request
 # Returns an HTML render
 def index(request):
-    return render(request, 'index.html')
+	return render(request, 'index.html')
 
 # Get the relation names in the database
 # Param: request - HTTP client request
