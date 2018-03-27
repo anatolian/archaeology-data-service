ae5071330dc2826010a8c1e7fbc807faef8ec144
CBesser <cbesser@seas.upenn.edu>
2018-03-27 14:34:14 -0400

    boto3

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -25,7 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello'
+    'hello',
+    'storages'
 ]
 LOGGING = {
     'version': 1,
