71bbf76ee709ab4011f80c8496c6819745d9aa3e
CBesser <cbesser@seas.upenn.edu>
2018-04-05 21:02:28 -0400

    added image routes back to urls

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     url(r'^get_find', hello.views.get_find, name = 'Fetch Find'),
     url(r'^set_weight', hello.views.set_weight, name = 'Set Weight'),
     url(r'^add_property', hello.views.add_property, name = 'Add Meta-variable'),
-    url(r'^get_property', hello.views.get_property, name = 'Get Meta-variable')#,
-    # url(r'^upload_file', hello.views.upload_file, name = 'Upload to S3'),
-    # url(r'^get_image_urls', hello.views.get_image_urls, name = 'Get S3 URLs')
+    url(r'^get_property', hello.views.get_property, name = 'Get Meta-variable'),
+    url(r'^upload_file', hello.views.upload_file, name = 'Upload to S3'),
+    url(r'^get_image_urls', hello.views.get_image_urls, name = 'Get S3 URLs')
 ]
