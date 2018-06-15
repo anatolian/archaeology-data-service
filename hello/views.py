6323bb90a0904c1e01bbcb0dcd7c20b786070661
CBesser <cbesser@seas.upenn.edu>
2018-06-15 15:26:02 +0400

    fixing path timestamps

--- hello/views.py
+++ hello/views.py
@@ -658,7 +658,7 @@ def insert_path(request):
 	query = query + "end_position_recording_ar_ratio, begin_timestamp, end_timestamp) VALUES (\'" + teamMember + "\', \'" + hemisphere + "\', " + zone + ", " + beginEasting
 	query = query + ", " + beginNorthing + ", " + endEasting + ", " + endNorthing + ", " + beginLongitude + ", " + beginLatitude + ", " + beginAltitude + ", \'" 
 	query = query + beginStatus + "\', " + beginARRatio + ", " + endLongitude + ", " + endLatitude + ", " + endAltitude + ", \'" + endStatus + "\', " + endARRatio
-	query = query + ", to_timestamp(CAST(" + beginTime + " AS DOUBLE PRECISION)), to_timestamp(CAST(" + endTime + " AS DOUBLE PRECISION)));"
+	query = query + ", to_timestamp(" + beginTime + " / 1000)), to_timestamp(" + endTime + " / 1000)));"
 	response = HttpResponse("Error: No records updated\n" + query, content_type = 'text/plain')
 	try:
 		cursor.execute(query)
